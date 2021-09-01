f2319d6af17aef81c8d71b3b91046bdef953369b
Rafael Acaio <akaiorafael@gmail.com>
2021-09-01 20:51:08 -0300

    New mensage

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,6 +7,9 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bonjiorno");
+
+            Console.ReadLine();
         }
     }
 }
